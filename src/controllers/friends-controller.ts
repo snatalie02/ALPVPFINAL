ab4ce98d85c1af781e0da5c09038236107a695cb
sharonnatalies <sharonsutanto25@gmail.com>
2025-12-15 16:48:36 +0700

    friends controller

---
+++ src/controllers/friends-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express"
+import { FriendsService } from "../services/friends-service"
+
+export class FriendsController {
+
+  static async getFriends(req: Request & { user?: any }, res: Response, next: NextFunction) {
+                                        // middleware alrd set the user but if the middleware fail makes it undefined if no optional crash
+                                        // current user id and username
+    try {
+      const currentUser = req.user // current user id and username
+      const response = await FriendsService.getFriends(currentUser.id) // get all the friends and their infos
+      // 200 success 
+      res.status(200).json({ data: response })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+}
